daf59521c692b8e5afd7159e53e724599350e7ea
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-02-15 13:00:00 +0000

    Added multi-threaded frame fetching

--- src/run.cpp
+++ src/run.cpp
@@ -9,10 +9,39 @@
 // Std
 #include <iostream>
 #include <thread>
+#include <mutex>
 
 namespace owl {
-	//void thread() {
-	//}
+	struct Params {
+		struct { float x = 0.0f, y = 0.0f; } eye[2];
+		float neck = 0.0f;
+	};
+
+	std::mutex frame_lock;
+	cv::Mat frame_buff[2];
+	cv::Mat* frame = nullptr;
+	
+	void frame_thread_func(cv::VideoCapture& vcap) {
+		// Switch between frame_buff[0] and frame_buff[1] to avoid copying
+		for (int fb = 0;;fb = (fb == 0) ? 1 : 0) {
+			cv::Mat tmp_frame;
+			// Check for errors when reading the next frame
+			if (!vcap.read(frame_buff[fb])) {
+				continue;
+			}
+			else {
+				// We have a valid frame. Switch it out.
+				frame_lock.lock();
+				frame = &frame_buff[fb];
+				frame_lock.unlock();
+			}
+
+			// Put the thread to sleep for a little while
+			std::this_thread::sleep_for(
+				std::chrono::milliseconds(10)
+			);
+		}
+	}
 
 	int run(std::string video_url, std::string pi_addr, int pi_port) {
 
@@ -23,16 +52,23 @@ namespace owl {
 			return 2;
 		}
 
+		std::thread frame_thread(frame_thread_func, std::ref(vcap));
+
 		// Main execution loop
 		bool running = true;
 		while (running) {
-			cv::Mat frame;
-			if (!vcap.read(frame)) {
-				std::cerr << "Failed to read frame from video capture." << std::endl;
-				return 3;
-			}
 
-			cv::imshow("Image", frame);
+			// Display the current frame
+			frame_lock.lock();
+			if (frame != nullptr)
+				cv::imshow("Image", *frame);
+			frame_lock.unlock();
+
+			// Wait for 1 ms or until a key is pressed
+			switch (auto key = cv::waitKey(1)) {
+				case 'q': running = false; break;
+				default: break;
+			};
 		}
 
 		// Deinit OpenCV
