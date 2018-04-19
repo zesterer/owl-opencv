17c0a493412e97ce81ec36d61724954971a2c32a
Joshua Barretto <joshua.s.barretto@gmail.com>
2018-04-19 16:49:50 +0100

    Added plan

--- src/run.cpp
+++ src/run.cpp
@@ -5,7 +5,7 @@
 
 // OpenCV
 #include <opencv2/core/core.hpp>
-//#include <opencv2/core/cvstd.hpp>
+#include <opencv2/core/cvstd.hpp>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
